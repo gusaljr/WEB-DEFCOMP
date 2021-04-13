5a31e5a2ca39fde071fe8f9b4a2193c4adcb2748
gusaljr <gusaljr6@gmail.com>
2021-04-13 22:19:05 +0200

    insertando carpeta Config

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ */
+class Conectar
+{
+
+  public static function  conexion()
+  {
+    $conexion = new mysqli("localhost","root","","infortisa");
+    return $conexion;
+  }
+}
+
+ ?>
